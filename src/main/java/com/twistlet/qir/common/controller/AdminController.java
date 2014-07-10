052377108750076287857a224df4826f38f8956a
Titi Wangsa bin Damhore <blacksnow666@gmail.com>
2014-07-11 07:08:59 +0800

    auto wired admin contoller constructor

--- src/main/java/com/twistlet/qir/common/controller/AdminController.java
+++ src/main/java/com/twistlet/qir/common/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.twistlet.qir.common.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,6 +12,7 @@ public class AdminController {
 
 	private final AdminService adminService;
 
+	@Autowired
 	public AdminController(final AdminService adminService) {
 		this.adminService = adminService;
 	}
