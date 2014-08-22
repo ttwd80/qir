89abb204646e082b5291a6c5b3477e717dc3c21e
Titi Wangsa bin Damhore <blacksnow666@gmail.com>
2014-08-22 18:07:29 +0800

    bcrypt password encoder

--- src/test/java/com/twistlet/qir/common/model/service/ElasticsearchDataInitServiceTest.java
+++ src/test/java/com/twistlet/qir/common/model/service/ElasticsearchDataInitServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.twistlet.qir.common.model.repository.RoleRepository;
 import com.twistlet.qir.common.model.repository.UserRepository;
@@ -21,7 +22,8 @@ public class ElasticsearchDataInitServiceTest {
 	@Before
 	public void init() {
 		MockitoAnnotations.initMocks(this);
-		sut = new ElasticsearchDataInitService(userRepository, roleRepository);
+		sut = new ElasticsearchDataInitService(new BCryptPasswordEncoder(),
+				userRepository, roleRepository);
 	}
 
 	@Test
