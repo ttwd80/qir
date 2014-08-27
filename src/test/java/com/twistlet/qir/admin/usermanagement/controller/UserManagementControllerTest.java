6582bb020da4f29fc97ade7a3fff29667fe9a24b
Titi Wangsa bin Damhore <blacksnow666@gmail.com>
2014-08-27 16:16:35 +0800

    unit test coverage for UserManagementController.remove

--- src/test/java/com/twistlet/qir/admin/usermanagement/controller/UserManagementControllerTest.java
+++ src/test/java/com/twistlet/qir/admin/usermanagement/controller/UserManagementControllerTest.java
@@ -1,10 +1,12 @@
 package com.twistlet.qir.admin.usermanagement.controller;
 
+import static org.hamcrest.core.IsEqual.*;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +14,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.twistlet.qir.admin.usermanagement.service.UserManagementService;
 import com.twistlet.qir.common.model.entity.User;
 
@@ -42,5 +46,25 @@ public class UserManagementControllerTest {
 		@SuppressWarnings("unchecked")
 		List<User> list = (List<User>) mav.getModel().get("list");
 		assertEquals(4, list.size());
+	}
+
+	@Test
+	public void testRemoveError() throws JsonProcessingException {
+		doThrow(new RuntimeException("ignore: fake exception on delete user"))
+				.when(userManagementService).remove("1");
+		Map<String, String> map = sut.remove("1");
+		ObjectMapper objectMapper = new ObjectMapper();
+		String actual = objectMapper.writeValueAsString(map);
+		String expected = "{\"status\":\"error\",\"message\":\"java.lang.RuntimeException: ignore: fake exception on delete user\"}";
+		assertThat(actual, equalTo(expected));
+	}
+
+	@Test
+	public void testRemoveSuccess() throws JsonProcessingException {
+		Map<String, String> map = sut.remove("1");
+		ObjectMapper objectMapper = new ObjectMapper();
+		String actual = objectMapper.writeValueAsString(map);
+		String expected = "{\"status\":\"success\",\"id\":\"1\"}";
+		assertThat(actual, equalTo(expected));
 	}
 }
