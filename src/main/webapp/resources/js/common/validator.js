575459e9e18243acb61f3fad867c4c8eda6d1d8b
Titi Wangsa bin Damhore <blacksnow666@gmail.com>
2014-08-30 10:59:25 +0800

    test validator.js

--- src/main/webapp/resources/js/common/validator.js
+++ src/main/webapp/resources/js/common/validator.js
@@ -1,46 +1,57 @@
+var create_validation;
 $(function() {
-	function create_validator_parameters(element) {
-		var obj = {};
-		$("div", element).each(function() {
-			var e = $(this);
-			var key = e.attr("class");
-			var val = e.text();
-			obj[key] = val;
-		});
-		return obj;
-	}
-	function create_validators(input) {
-		var obj = {
-
-		};
-		$("div.validators > div", input.parent()).each(function() {
-			var e = $(this);
-			var c = e.attr("class");
-			obj[c] = create_validator_parameters(e);
-		});
-		return obj;
-	}
-	function create_fields_object(form) {
-		var fields = {};
-		$("input.bv-input", form).each(function(index, input) {
-			var validators = {
-				"validators" : create_validators($(this))
-			};
-			fields[$(this).attr("name")] = validators;
-		});
-		return fields;
-
-	}
 	$("form.bv-form").each(function(index, element) {
+		$form = $(element);
+		create_validation = create_form_validation;
+		var obj = create_form_validation($form);
+		$form.bootstrapValidator(obj);
+	})
+
+	var create_form_validation = function(form) {
 		var obj = {
 			"feedbackIcons" : {
 				valid : 'glyphicon glyphicon-ok',
 				invalid : 'glyphicon glyphicon-remove',
 				validating : 'glyphicon glyphicon-refresh'
 			},
-			"fields" : create_fields_object($(this))
+			"fields" : create_fields_object(form)
 		};
+		return obj;
+	}
+	function create_fields_object(form) {
+		var fields = {};
+		$("input.bv-input", form).each(function(index, element) {
+			var validators = {
+				"validators" : create_validators($(element))
+			};
+			fields[$(this).attr("name")] = validators;
+		});
+		return fields;
 
-		$(this).bootstrapValidator(obj);
-	});
+	}
+
+	function create_validators(element) {
+		var obj = {
+
+		};
+		var p = element.parent();
+		$("div.validators > div", p).each(function(index, element) {
+			var e = $(element);
+			var c = e.attr("class");
+			obj[c] = create_validator_parameters(e);
+		});
+		return obj;
+	}
+
+	function create_validator_parameters(div) {
+		var obj = {};
+		$("div", div).each(function(index, element) {
+			var e = $(element);
+			var key = e.attr("class");
+			var val = e.text();
+			obj[key] = val;
+		});
+		return obj;
+	}
+
 });
